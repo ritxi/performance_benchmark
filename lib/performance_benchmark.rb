be1a60b911111fe4ce37d9bc55557bebea1f42f4
Ricard Forniol Agustí <ricard@forniol.cat>
2015-09-22 12:35:58 +0200

    Basic DSL with let, prepare and run methods

--- lib/performance_benchmark.rb
+++ lib/performance_benchmark.rb
@@ -1,4 +1,62 @@
 require "performance_benchmark/version"
+require 'benchmark'
+require 'singleton'
 
 class PerformanceBenchmark
+  include Singleton
+
+  def self.benchmark(name, &block)
+    instance.benchmark(name, &block)
+  end
+
+  def self.results
+    instance.results
+  end
+
+  def initialize
+    @benchmark = {}
+  end
+
+  def benchmark(name, &block)
+    environment = Environment.new
+    environment.instance_exec(&block)
+    environment.prepare
+    @benchmark[name] = environment.run
+  end
+
+  def results
+    @benchmark
+  end
+
+  class Environment
+    def initialize
+      @let ||= {}
+    end
+
+    def let(name, &block)
+      @let[name.to_sym] = block
+    end
+
+    def prepare(&block)
+      if block
+        @prepare = block
+      else
+        @prepare && @prepare.call
+      end
+    end
+
+    def run(&block)
+      return Benchmark.realtime(&@run) if @run && !block
+      @run = block
+    end
+
+    def method_missing(method_sym, *arguments, &block)
+      # the first argument is a Symbol, so you need to_s it if you want to pattern match
+      if @let.keys.include?(method_sym)
+        @let[method_sym].call
+      else
+        super
+      end
+    end
+  end
 end
--- spec/performance_benchmark_spec.rb
+++ spec/performance_benchmark_spec.rb
@@ -1,11 +1,100 @@
 require 'spec_helper'
 
 describe PerformanceBenchmark do
-  it 'has a version number' do
-    expect(PerformanceBenchmark::VERSION).not_to be nil
+  describe 'PerformanceBenchmark::VERSION' do
+    subject { PerformanceBenchmark::VERSION }
+    it { is_expected.not_to be_nil }
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'benchmark' do
+    class Foo
+      class << self
+        def bar
+          'hello'
+        end
+
+        def buff
+          'bye'
+        end
+
+        def ahh!
+          'Munch'
+        end
+      end
+    end
+
+    let(:benchmark) do
+      PerformanceBenchmark.benchmark 'Test!' do
+        let(:foo) { Foo.bar }
+        prepare { Foo.buff }
+
+        run { foo; Foo.ahh! }
+      end
+    end
+
+    describe '#benchmark' do
+      before do
+        expect(Foo).to receive(:bar).once
+        expect(Foo).to receive(:buff).once
+        expect(Foo).to receive(:ahh!).once
+      end
+
+      it { benchmark }
+    end
+
+    describe '#results' do
+      before { benchmark }
+      subject { PerformanceBenchmark.results }
+
+      it { expect(subject.keys).to include('Test!') }
+      it { expect(subject.values.first).to be_kind_of(Float) }
+    end
+  end
+
+  describe PerformanceBenchmark::Environment do
+    let(:environment) { described_class.new }
+
+    describe '#let' do
+      subject { environment.hello }
+
+      before { environment.let(:hello) { 'foooo' } }
+
+      it { is_expected.to eq('foooo') }
+    end
+
+    describe '#run' do
+      let(:run_block) { -> { 'hello' } }
+      subject { environment.run(&run_block) }
+
+      it { is_expected.to eq(run_block) }
+
+      context 'run it' do
+        before { environment.run(&run_block) }
+        subject { environment.run }
+
+        it { is_expected.to be_kind_of(Float) }
+      end
+    end
+
+    describe '#prepare' do
+      let(:run_block) { -> { 'hello' } }
+      subject { environment.prepare(&run_block) }
+
+      it { is_expected.to eq(run_block) }
+
+      context 'run it' do
+        subject { environment.prepare }
+
+        context 'with block' do
+          before { environment.prepare(&run_block) }
+
+          it { is_expected.to eq('hello') }
+        end
+
+        context 'without block' do
+          it { is_expected.to be_nil }
+        end
+      end
+    end
   end
 end
